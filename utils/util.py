8392f1dab60387971a9948aa7b37fca50638a132
Leon <xtleon.xl@gmail.com>
2021-05-06 04:48:14 +0600

    Added utils base structure

---
+++ utils/util.py
@@ -0,0 +1,18 @@
+"""
+Created by Mazharul Islam Leon
+
+"""
+def test():
+    """
+        For testing purpose
+    """
+    print("[Utils]: Testing method call")
+
+def data_loader(dataset_name,):
+    """
+    This is dataloader method
+    params:
+        dataset_name: it will take a dataset name 
+    ret:
+        return the trainloader, valloader and testloader
+    """
