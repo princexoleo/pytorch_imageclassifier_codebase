9caadf475af8d89ae2a6a6d0079efb071f80665c
Leon <xtleon.xl@gmail.com>
2021-05-11 00:45:55 +0600

    Modified the structures of this projects

---
+++ leo/main.py
@@ -0,0 +1,20 @@
+"""
+Created by Mazharul Islam Leon
+
+"""
+import os
+import argparse
+
+from utils.util import test as ut_test 
+from utils import args
+from models.cnn_simple import test as cnn_test
+
+def test():
+    """
+        For testing purpose
+    """
+    print("[Main]: Testing method call")
+
+#
+if __name__ == "__main__":
+    pass
